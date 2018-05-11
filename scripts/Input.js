0cacab4ed366bd9060c01c58da4004afee129fab
Daniel Britt <dbritt@csumb.edu>
2018-05-11 02:49:25 +0000

    removed word limit

--- scripts/Input.js
+++ scripts/Input.js
@@ -22,7 +22,7 @@ export class Input extends React.Component {
         });
     }
     componentDidMount() {
-         Socket.on('player', (data) => {
+        Socket.on('player', (data) => {
             console.log(data['text'])
             console.log("ENTERED PLAYER FUNCTION")
             var player = document.getElementById('player');
@@ -30,20 +30,20 @@ export class Input extends React.Component {
             // In browsers that don’t yet support this functionality,
             // playPromise won’t be defined.
             if (promise !== undefined) {
-              promise.then(function() {
-                // Automatic playback started!
-              }).catch(function(error) {
-                // Automatic playback failed.
-                // Show a UI element to let the user manually start playback.
-              });
+                promise.then(function() {
+                    // Automatic playback started!
+                }).catch(function(error) {
+                    // Automatic playback failed.
+                    // Show a UI element to let the user manually start playback.
+                });
             }
             console.log("AFTER GETTING PLAYER FROM ID")
             var text = encodeURIComponent(data['text']);
             console.log(text);
             var fromLang = data['fromLang'];
             var toLang = data['toLang'];
-            var returnVal = '/read?text=' + text + '&fromLang=' + fromLang+ '&toLang=' + toLang;
-            
+            var returnVal = '/read?text=' + text + '&fromLang=' + fromLang + '&toLang=' + toLang;
+
             //var returnVal = '/read?text=' + data['text'];
             console.log("AFTER AJAX /READ CALL")
             console.log(returnVal);
@@ -54,33 +54,33 @@ export class Input extends React.Component {
             // In browsers that don’t yet support this functionality,
             // playPromise won’t be defined.
             if (promise !== undefined) {
-              promise.then(function() {
-                // Automatic playback started!
-              }).catch(function(error) {
-                // Automatic playback failed.
-                // Show a UI element to let the user manually start playback.
-              });
+                promise.then(function() {
+                    // Automatic playback started!
+                }).catch(function(error) {
+                    // Automatic playback failed.
+                    // Show a UI element to let the user manually start playback.
+                });
             }
         });
     }
     handleSubmit(event) {
-        if($("#textarea").val().length < 160){
-            console.log("The current message is!!! " + this.state.value)
-            //this emits to main.js
-            Socket.emit('message', {
-                data: this.state.value
-            });
-            
-            // alert('A name was submitted: ' + this.state.value);
-            $("#textarea").val('')
-            event.preventDefault();
-            this.setState({
-                count: 0
-            });
-        } else {
-            alert("Message must be shorter than 160 characters.")
-            event.preventDefault();
-        }
+        // if($("#textarea").val().length < 160){
+        console.log("The current message is!!! " + this.state.value)
+        //this emits to main.js
+        Socket.emit('message', {
+            data: this.state.value
+        });
+
+        // alert('A name was submitted: ' + this.state.value);
+        $("#textarea").val('')
+        event.preventDefault();
+        this.setState({
+            count: 0
+        });
+        // } else {
+        //     alert("Message must be shorter than 160 characters.")
+        //     event.preventDefault();
+        // }
     }
 
     render() {
@@ -91,7 +91,6 @@ export class Input extends React.Component {
                   <input placeholder="Message..." id="textarea" type="text" onChange={this.handleChange} />
                 </label>
               </form>
-              <div id="charCount">{this.state.count}/160</div>
             </div>
         );
     }
